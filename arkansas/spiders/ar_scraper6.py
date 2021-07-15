408f41b44d12ae899635814fcd6cd3e45f580f0e
Akash Chauhan <1993akash@gmail.com>
2021-07-15 23:45:41 +0530

    different spiders have been added for 10 different pages

--- arkansas/spiders/ar_scraper6.py
+++ arkansas/spiders/ar_scraper6.py
@@ -5,16 +5,16 @@ domain = "https://www.arkleg.state.ar.us"
 
 
 class ArScraperSpider(scrapy.Spider):
-    name = 'ar_1'
+    name = 'ar_6'
     # allowed_domains = ['https://www.arkleg.state.ar.us/Acts/SearchByRange?start=0']
-    start_urls = ['https://www.arkleg.state.ar.us/Acts/SearchByRange?start=0&startAct=1&endAct=188&ddBienniumSession=2021%2F2021R#SearchResults']
+    start_urls = ['https://www.arkleg.state.ar.us/Acts/SearchByRange?start=100&startAct=1&endAct=188&ddBienniumSession=2021%2F2021R#SearchResults']
 
     def parse(self, response):
         try:
             name = response.xpath("//div[@class='col-xs-5 col-sm-5 col-md-5']/text()").getall()
             bill = response.xpath('//div[@aria-colindex="3"]/a/text()').getall()
             links = response.xpath('//div[@class="col-md-1 col-lg-1 meetingButtons mobileButtons"]/a/@href').getall()
-            with open(R'E:\arkansas_crawl\arkansas\output_1.csv',"w",newline=None, encoding='utf8') as myfile:
+            with open(R'E:\arkansas_crawl\arkansas\output_6.csv',"w",newline=None, encoding='utf8') as myfile:
                 csvwriter = csv.writer(myfile,delimiter=",",lineterminator='\r')
                 csvwriter.writerow(['Doc Name','Bill NO.','Doc URL'])
                 for a,b,c in zip(name,bill,links):
